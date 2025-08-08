8b6bbdd3967f00e609b0ee924becaa12c7361bce
xiaomin cai <syoubin@gmail.com>
2025-08-08 14:15:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/next.config.ts
@@ -0,0 +1,37 @@
+/** @type {import('next').NextConfig} */
+const withNextIntl = require('next-intl/plugin')('./src/i18n.ts');
+
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = withNextIntl(nextConfig);
