c4d587516ec1a1d7440c1b37caf54100102e0577
xiaomin cai <syoubin@gmail.com>
2025-08-08 14:16:17 +0000

    Error: Error checking Cross-Origin-Opener-Policy: "HTTP error! status: 4

--- src/next.config.ts
+++ src/next.config.ts
@@ -22,7 +22,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/:path*',
+        source: '/(.*)',
         headers: [
           {
             key: 'Cross-Origin-Opener-Policy',
