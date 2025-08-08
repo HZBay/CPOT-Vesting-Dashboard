495c3aaf07e5ca3fb26065fea9036b9f049baba9
xiaomin cai <syoubin@gmail.com>
2025-08-08 09:43:32 +0000

    1405e4f952e2630cf406cb00bc9cfb72

--- src/components/providers/web3-provider.tsx
+++ src/components/providers/web3-provider.tsx
@@ -26,7 +26,7 @@ const config = getDefaultConfig({
   appName: 'CPOT Vesting Dashboard',
   // IMPORTANT: Replace "YOUR_PROJECT_ID" with your actual WalletConnect Project ID
   // and add your app's domain to the allowlist at https://cloud.walletconnect.com/
-  projectId: 'c9535761828f09004f141133379058b2', 
+  projectId: '1405e4f952e2630cf406cb00bc9cfb72', 
   chains: [hashkeyTestnet],
   ssr: true,
 });
